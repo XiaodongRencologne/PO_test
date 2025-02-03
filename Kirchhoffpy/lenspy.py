e3e85ffa73474b286f7b866827808547adf4d3a4
XiaodongRencologne <xiaodongrenpmo@gmail.com>
2025-02-03 18:28:11 +0000

    lens view

--- Kirchhoffpy/lenspy.py
+++ Kirchhoffpy/lenspy.py
@@ -84,6 +84,8 @@ class simple_Lens():
     def __init__(self,
                  n,thickness, D,
                  surface_file1, surface_file2,
+                 widget,
+                 coord,
                  name = 'simplelens'):
         self.name = name
         self.n = n
@@ -91,6 +93,7 @@ class simple_Lens():
         self.diameter = D
         self.surf_fnc1 = read_rsf(surface_file1)
         self.surf_fnc2 = read_rsf(surface_file2)
+        self.coord = coord
 
         # define surface for sampling or for 3Dview
         self.f1 = Coord()
@@ -100,6 +103,8 @@ class simple_Lens():
         # 3D view
         # Analysis method
         self.method = None
+        self.widget = widget
+        '''
         self.widget=pv.Plotter(notebook=True)
         _ = self.widget.add_axes(
             line_width=5,
@@ -110,7 +115,7 @@ class simple_Lens():
             label_size=(0.4, 0.16),
         )
         _ = self.widget.add_bounding_box(line_width=5, color='black')
-
+        '''
     def analysis(self,N1,N2):
         if self.method == None:
             print('Please define the analysis methods!!!')
@@ -135,7 +140,7 @@ class simple_Lens():
                                         f2_N[0],f2_N[1],
                                         0,2*np.pi,f2_N[2],
                                         Phi_type=phi_type)
-        elif Sampling_type = 'rectangle':
+        elif Sampling_type == 'rectangle':
             f1.x, f1.y1, f1.w = Gauss_L_quadrs2d(-self.diameter/2,self.diameter/2,1,f1_N[0],
                                           -self.diameter/2,self.diameter/2,1,f1_N[1])
             f2.x, f2.y1, f2.w = Gauss_L_quadrs2d(-self.diameter/2,self.diameter/2,1,f2_N[0],
@@ -155,21 +160,26 @@ class simple_Lens():
         return f1,f2,f1_n,f2_n,w1,w2
         """
     def view(self,N1 = 101,N2 =101):
-        if self.name+'_face1' in self.widget.actoers.keys():
+        if self.name+'_face1' in self.widget.actors.keys():
             self.widget.remove_actor(self.widget.actors[self.name+'_face1'])
         x1 = np.linspace(0,self.diameter/2,N1)
         x2 = np.linspace(0,self.diameter/2,N2)
-        z1 = self.surf_fnc1(r1,0)
-        z2 = self.surf_fnc2(r2,0)
-        p1 = pv.PolyData(np.column_stack((x1,np.zeros(x1.shape),z1)))
-        p2 = pv.PolyData(np.column_stack((x2,np.zeros(x2.shape),z2)))
+        z1, n1 = self.surf_fnc1(x1/10,0)
+        z2, n2 = self.surf_fnc2(x2/10,0)
+        print(z1.max(),z1.min())
+        self.z1 = z1*10 +self.coord
+        self.x1 = x1
+        self.z2 = -z2*10 +self.coord
+        self.x2 = x2
+        p1 = pv.PolyData(np.column_stack((x1,np.zeros(x1.shape),self.z1)))
+        p2 = pv.PolyData(np.column_stack((x2,np.zeros(x2.shape),self.t+self.z2)))
         p1 = p1.delaunay_2d()
         p2 = p2.delaunay_2d()
 
-        view_face1 = p1.extrude_rotate(resolution=60)
-        view_face2 = p2.extrude_rotate(resolution=60)
+        view_face1 = p1.extrude_rotate(resolution=100)
+        view_face2 = p2.extrude_rotate(resolution=100)
 
-        self.widget.add_mesh(view_face1, color= 'lightblue' ,opacity= 0.8,name = self.name+'_face1')
-        self.widget.add_mesh(view_face2, color= 'lightblue' ,opacity= 0.8,name = self.name+'_face2')
-        self.widget.show()
+        self.widget.add_mesh(view_face1, color= 'lightblue' ,opacity= 1,name = self.name+'_face1')
+        self.widget.add_mesh(view_face2, color= 'lightblue' ,opacity= 1,name = self.name+'_face2')
+        #self.widget.show()
         
